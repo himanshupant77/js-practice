231638305edc4e1e4ea8805a5d50bde0e58db058
Himanshu Pant <110613035+himanshupant77@users.noreply.github.com>
2024-07-18 17:17:44 +0000

    config done

--- basic/objects.js
+++ basic/objects.js
@@ -8,13 +8,25 @@ const User = {
      location:"ghaziabad",
      isLoggedIn: false
 }
-console.log(User.email)
-console.log(User["email"])        //2nd way to call object
-console.log(User["full name"])
+// console.log(User.email)
+// console.log(User["email"])        //2nd way to call object
+// console.log(User["full name"])
 
 User.email="himanshu@yahoo.com"   //changes value in object
-console.log(User.email)
+// console.log(User.email)
 
-Object.freeze(User)               //freezes the object , no change allowed
-User.email="himanshu@hotmail.com"  // changes will not been make now 
-console.log(User.email)
+// Object.freeze(User)               //freezes the object , no change allowed
+// User.email="himanshu@hotmail.com"  // changes will not been make now 
+// console.log(User.email)
+// console.log(User.isLoggedIn)
+
+
+User.greetings=function(){
+    console.log("Hello User")
+}
+User.greetingsTwo=function(){
+    console.log(`Hello User ${this["full name"]}`)
+}
+
+console.log(User.greetings())
+console.log(User.greetingsTwo())
