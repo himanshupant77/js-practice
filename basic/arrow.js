e5437376f6b51636e6e78b5f1679135d66c0b5c5
Himanshu Pant <110613035+himanshupant77@users.noreply.github.com>
2024-07-20 17:43:31 +0000

    arrow function

---
+++ basic/arrow.js
@@ -0,0 +1,34 @@
+const user={
+    username:"himanshu",
+    price:999,
+    
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`)
+    }
+}
+
+// user.welcomeMessage()
+// user.username="sam"
+// user.welcomeMessage()
+
+
+const one = function(){
+    let username="himanshu"
+    console.log(this)
+}
+// one()
+
+// const one = () => {                   //arrow function
+//     let username="himanshu"
+//     console.log(this)
+// }
+// one()
+
+
+// const sum = (num1,num2) => {         //normal arrow function
+//      return num1+num2
+// } 
+// console.log(sum(4,5))
+
+const sum1 = (num1,num2) => num1+num2   //implicit return (no need to use return keyword)
+console.log(sum1(4,5))
