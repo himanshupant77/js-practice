820f3197e908428639cb96c3e65ffa4e01abd5f3
Himanshu Pant <110613035+himanshupant77@users.noreply.github.com>
2024-07-20 12:07:21 +0000

    scope update

---
+++ basic/scope.js
@@ -0,0 +1,34 @@
+
+if (true){
+
+    let a=10
+    const b=20
+    var c=30
+    
+}
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+if (true) {
+    const firstname="himanshu"                
+    if (firstname=="himanshu") {               // functions called inside a functions  
+       const lastname=" pant"                 // child can take values from parent but not vice versa 
+     // console.log(firstname+lastname)      // this property is called closure.
+    }
+    // console.log(lastname);               // will give error because declared out of scope   
+}
+
+//  INTERESTING ***********
+ 
+addone()                          //function declaration this type supports hoisting
+function addone(num){
+    return num +1
+}
+
+
+addTwo()                          //declaring function this type dosnt support hoisting
+const addTwo= function(num){
+    return num + 2
+}  
