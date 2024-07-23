2337a4378a2ccd67b78aed2d0db50909d6e3bff3
Himanshu Pant <110613035+himanshupant77@users.noreply.github.com>
2024-07-23 17:11:17 +0000

    switch statement

---
+++ control_flow/switch.js
@@ -0,0 +1,20 @@
+const month=3
+
+switch (month) {
+    case 1:
+        console.log("Jan")
+        break;                 //break very important to break control flow
+    case 2:
+        console.log("Feb")
+        break;
+    case 3:
+        console.log("Mar")
+        break;
+    case 4:
+        console.log("April")
+        break;
+
+    default:
+        console.log("default month")
+        break;
+}
